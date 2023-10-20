b80fa90f666ea2829c6e68164ce174a0cc13f190
mlcato123 <redviter69@gmail.com>
2023-10-21 00:27:45 +0300

    done

---
+++ src/main/java/App.java
@@ -0,0 +1,15 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat one = (Cat) applicationContext.getBean("cat");
+        Cat two = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Первая пара - " + (bean1 == bean2));
+        System.out.println("Вторая пара - " + (one == two));
+    }
+}
